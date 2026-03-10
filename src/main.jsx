f0d44eb1206e1dbce388511f3cd60ef5f8c31833
lkarimex27-dotcom <lkarimex27@gmail.com>
2026-03-10 10:54:42 -0500

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { CartProvider } from './features/auth/Hooks/CartContext';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+  <SpeedInsights>
   <CartProvider>
   <StrictMode>
     <App />
   </StrictMode>
   </CartProvider>
+  </SpeedInsights>
 )
